417387df6eb3741ff2e5b632b70eb3763a4cdea9
thephez <thephez@users.noreply.github.com>
2017-03-07 10:40:11 -0500

    Initial commit * No authentication - only works for devices allowing access w/out user/pwd  - Loads Onvif data from at least Bosch cameras  - Successfully go to a preset on Bosch camera

---
+++ Onvif Interface/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Onvif_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetOnvifInfo = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.lblDeviceTime = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblFirmware = new System.Windows.Forms.Label();
+            this.lblSerial = new System.Windows.Forms.Label();
+            this.lblHwID = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.gbxDeviceInfo = new System.Windows.Forms.GroupBox();
+            this.lbxCapabilities = new System.Windows.Forms.ListBox();
+            this.lblCapabilities = new System.Windows.Forms.Label();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1.SuspendLayout();
+            this.gbxDeviceInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetOnvifInfo
+            // 
+            this.btnGetOnvifInfo.Location = new System.Drawing.Point(37, 47);
+            this.btnGetOnvifInfo.Name = "btnGetOnvifInfo";
+            this.btnGetOnvifInfo.Size = new System.Drawing.Size(109, 30);
+            this.btnGetOnvifInfo.TabIndex = 2;
+            this.btnGetOnvifInfo.Text = "Get Device Info";
+            this.btnGetOnvifInfo.UseVisualStyleBackColor = true;
+            this.btnGetOnvifInfo.Click += new System.EventHandler(this.btnGetOnvifInfo_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(37, 21);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(110, 20);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // lblDeviceTime
+            // 
+            this.lblDeviceTime.AutoSize = true;
+            this.lblDeviceTime.Location = new System.Drawing.Point(6, 88);
+            this.lblDeviceTime.Name = "lblDeviceTime";
+            this.lblDeviceTime.Size = new System.Drawing.Size(36, 13);
+            this.lblDeviceTime.TabIndex = 2;
+            this.lblDeviceTime.Text = "Time: ";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssLbl
+            // 
+            this.tssLbl.Name = "tssLbl";
+            this.tssLbl.Size = new System.Drawing.Size(39, 17);
+            this.tssLbl.Text = "Status";
+            // 
+            // lblFirmware
+            // 
+            this.lblFirmware.AutoSize = true;
+            this.lblFirmware.Location = new System.Drawing.Point(6, 34);
+            this.lblFirmware.Name = "lblFirmware";
+            this.lblFirmware.Size = new System.Drawing.Size(52, 13);
+            this.lblFirmware.TabIndex = 4;
+            this.lblFirmware.Text = "Firmware:";
+            // 
+            // lblSerial
+            // 
+            this.lblSerial.AutoSize = true;
+            this.lblSerial.Location = new System.Drawing.Point(6, 52);
+            this.lblSerial.Name = "lblSerial";
+            this.lblSerial.Size = new System.Drawing.Size(46, 13);
+            this.lblSerial.TabIndex = 5;
+            this.lblSerial.Text = "Serial #:";
+            // 
+            // lblHwID
+            // 
+            this.lblHwID.AutoSize = true;
+            this.lblHwID.Location = new System.Drawing.Point(6, 70);
+            this.lblHwID.Name = "lblHwID";
+            this.lblHwID.Size = new System.Drawing.Size(70, 13);
+            this.lblHwID.TabIndex = 6;
+            this.lblHwID.Text = "Hardware ID:";
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(6, 16);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(39, 13);
+            this.lblModel.TabIndex = 7;
+            this.lblModel.Text = "Model:";
+            // 
+            // gbxDeviceInfo
+            // 
+            this.gbxDeviceInfo.Controls.Add(this.lblCapabilities);
+            this.gbxDeviceInfo.Controls.Add(this.lbxCapabilities);
+            this.gbxDeviceInfo.Controls.Add(this.lblModel);
+            this.gbxDeviceInfo.Controls.Add(this.lblHwID);
+            this.gbxDeviceInfo.Controls.Add(this.lblSerial);
+            this.gbxDeviceInfo.Controls.Add(this.lblFirmware);
+            this.gbxDeviceInfo.Controls.Add(this.lblDeviceTime);
+            this.gbxDeviceInfo.Location = new System.Drawing.Point(294, 21);
+            this.gbxDeviceInfo.Name = "gbxDeviceInfo";
+            this.gbxDeviceInfo.Size = new System.Drawing.Size(260, 281);
+            this.gbxDeviceInfo.TabIndex = 8;
+            this.gbxDeviceInfo.TabStop = false;
+            this.gbxDeviceInfo.Text = "Device Info";
+            // 
+            // lbxCapabilities
+            // 
+            this.lbxCapabilities.FormattingEnabled = true;
+            this.lbxCapabilities.Location = new System.Drawing.Point(9, 139);
+            this.lbxCapabilities.Name = "lbxCapabilities";
+            this.lbxCapabilities.Size = new System.Drawing.Size(245, 134);
+            this.lbxCapabilities.TabIndex = 9;
+            // 
+            // lblCapabilities
+            // 
+            this.lblCapabilities.AutoSize = true;
+            this.lblCapabilities.Location = new System.Drawing.Point(9, 123);
+            this.lblCapabilities.Name = "lblCapabilities";
+            this.lblCapabilities.Size = new System.Drawing.Size(112, 13);
+            this.lblCapabilities.TabIndex = 10;
+            this.lblCapabilities.Text = "Supported Capabilities";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(154, 21);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(66, 20);
+            this.numPort.TabIndex = 1;
+            this.numPort.Value = new decimal(new int[] {
+            8251,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.gbxDeviceInfo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnGetOnvifInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.gbxDeviceInfo.ResumeLayout(false);
+            this.gbxDeviceInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetOnvifInfo;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label lblDeviceTime;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssLbl;
+        private System.Windows.Forms.Label lblFirmware;
+        private System.Windows.Forms.Label lblSerial;
+        private System.Windows.Forms.Label lblHwID;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.GroupBox gbxDeviceInfo;
+        private System.Windows.Forms.ListBox lbxCapabilities;
+        private System.Windows.Forms.Label lblCapabilities;
+        private System.Windows.Forms.NumericUpDown numPort;
+    }
+}
+
---
+++ Onvif Interface/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+using Onvif_Interface.OnvifDeviceManagementServiceReference;
+using System.ServiceModel.Discovery;
+using System.IO;
+using Onvif_Interface.OnvifPtzServiceReference;
+
+namespace Onvif_Interface
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetOnvifInfo_Click(object sender, EventArgs e)
+        {
+            tssLbl.Text = "Scanning device";
+            btnGetOnvifInfo.Enabled = false;
+            UseWaitCursor = true;
+            try
+            {
+                GetOnvifInfo(txtIP.Text, (int)numPort.Value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Exception: {0}", ex.Message));
+                MessageBox.Show(string.Format("Exception: {0}", ex.Message), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                btnGetOnvifInfo.Enabled = true;
+                UseWaitCursor = false;
+            }
+        }
+
+        private void GetOnvifInfo(string ip, int port = 80)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/device_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            // Add our custom behavior - this require the Microsoft WSE 3.0 SDK
+            //PasswordDigestBehavior behavior = new PasswordDigestBehavior(CameraASCIIStringLogin, CameraASCIIStringPassword);
+
+            using (DeviceClient client = new DeviceClient(bind, serviceAddress))
+            {
+                client.Endpoint.Behaviors.Add(new EndpointDiscoveryBehavior());
+
+                // We can now ask for information
+                // ONVIF application programmer guide (5.1.3) suggests checking time first 
+                // (no auth required) so time offset can be determined (needed for auth if applicable)
+                GetDeviceTime(client);
+
+                GetDeviceInfo(client);
+
+                GetCapabilities(client);
+
+                PTZTest(client, ip, port);
+            }
+        }
+
+        private void GetDeviceTime(DeviceClient client)
+        {
+            try
+            {
+                SystemDateTime dt = client.GetSystemDateAndTime();
+                string date = string.Format("{0:0000}-{1:00}-{2:00}", dt.UTCDateTime.Date.Year, dt.UTCDateTime.Date.Month, dt.UTCDateTime.Date.Day);
+                string time = string.Format("{0:00}:{1:00}:{2:00}", dt.UTCDateTime.Time.Hour, dt.UTCDateTime.Time.Minute, dt.UTCDateTime.Time.Second);
+                lblDeviceTime.Text = string.Format("Device Time: {0} {1}", date, time);
+                //File.AppendAllText("info.txt", string.Format("\n\nDate and Time from: {0}:{1} [UTC Date: {2}, UTC Time: {3}]", ip, port, date, time));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void GetDeviceInfo(DeviceClient client)
+        {
+            string model;
+            string fwversion;
+            string serialno;
+            string hwid;
+            try
+            {
+                client.GetDeviceInformation(out model, out fwversion, out serialno, out hwid);
+                string deviceInfo = string.Format("\nDevice: {0} [Firmware: {1}, Serial #: {2}, Hardware ID: {3}]\n", model, fwversion, serialno, hwid);
+                lblModel.Text = string.Format("Model: {0}", model);
+                lblFirmware.Text = string.Format("Firmware: {0}", fwversion);
+                lblSerial.Text = string.Format("Serial #: {0}", serialno);
+                lblHwID.Text = string.Format("Hardware ID: {0}", hwid);
+
+                tssLbl.Text = string.Format("{0} - Device info retrieved", System.DateTime.Now);
+                Console.WriteLine(deviceInfo);
+                //File.AppendAllText("info.txt", string.Format("\nDevice: {0} ({4}:{5}) [Firmware: {1}, Serial #: {2}, Hardware ID: {3}]\n", model, fwversion, serialno, hwid, ip, port));
+            }
+            catch (Exception ex) when (ex.InnerException.Message == "The remote server returned an error: (401) Unauthorized.")
+            {
+                // Authentication required
+                tssLbl.Text = string.Format("{0} Authentication failure. Unable to log into device.", System.DateTime.Now); // - {0}", ex.Message);
+                throw new Exception(string.Format("Authentication failure - {0}", ex.Message), ex);
+            }
+        }
+
+        private void GetCapabilities(DeviceClient client)
+        {
+            lbxCapabilities.Items.Clear();
+
+            //capabilities = client.GetCapabilities(new CapabilityCategory[] { CapabilityCategory.All });
+
+            //if (capabilities.Analytics != null) { lbxCapabilities.Items.Add("Analytics"); }
+            //if (capabilities.Events != null) { lbxCapabilities.Items.Add("Events"); }
+            //if (capabilities.Extension != null) { lbxCapabilities.Items.Add("Extension"); }
+            //if (capabilities.Imaging != null) { lbxCapabilities.Items.Add("Imaging"); }
+            //if (capabilities.Media != null) { lbxCapabilities.Items.Add("Media"); }
+            //if (capabilities.PTZ != null) { lbxCapabilities.Items.Add("PTZ"); }
+
+            //lbxCapabilities.Items.Add("");
+
+            Service[] svc = client.GetServices(IncludeCapability: true);
+            foreach (Service s in svc)
+            {
+                Console.WriteLine(s.XAddr + " " + s.Capabilities.NamespaceURI + " " + s.Namespace);
+                lbxCapabilities.Items.Add(string.Format("{0}", s.Namespace));
+                foreach (System.Xml.XmlNode x in s.Capabilities)
+                {
+                    Console.WriteLine(string.Format("\t{0}", x.LocalName));
+                    lbxCapabilities.Items.Add(string.Format("    {0}", x.LocalName));
+                    if (x.Attributes.Count > 0)
+                    {
+                        foreach (System.Xml.XmlNode a in x.Attributes)
+                        {
+                            Console.WriteLine(string.Format("\t\t{0} = {1}", a.Name, a.Value));
+                            lbxCapabilities.Items.Add(string.Format("        {0} = {1}", a.Name, a.Value));
+                        }
+                    }
+                }
+            }
+
+            DeviceServiceCapabilities dsc = client.GetServiceCapabilities();
+
+        }
+
+        private void PTZTest(DeviceClient client, string ip, int port)
+        {
+            if (lbxCapabilities.Items.Contains("http://www.onvif.org/ver20/ptz/wsdl"))
+            {
+                // Create Media object
+                OnvifMediaServiceReference.MediaClient mediaService = GetOnvifMediaClient(ip, port);
+
+                // Create PTZ object
+                PTZClient ptzService = GetOnvifPTZClient(ip, port); // new PTZClient(client.Endpoint.Binding, client.Endpoint.Address);
+                Console.WriteLine(ptzService);
+
+                // Get target profile
+                string profileToken = "0";
+                OnvifMediaServiceReference.Profile mediaProfile = mediaService.GetProfile(profileToken);
+
+                // Get Presets
+                PTZPreset[] presets = ptzService.GetPresets(profileToken);
+
+                // Get Status
+                PTZStatus status = ptzService.GetStatus(profileToken);
+
+                //// Get Nodes
+                //OnvifPtzServiceReference.PTZNode[] nodes = ptz.GetNodes();
+                //Console.WriteLine(nodes.Length);
+
+                //foreach (OnvifPtzServiceReference.PTZNode n in nodes)
+                //{
+                //    lbxCapabilities.Items.Add("PTZ " + n.Name);
+                //    File.AppendAllText("ptz.txt", string.Format("\nPTZ node - Name: {0}, Presets: {1}", n.Name, n.MaximumNumberOfPresets));
+                //}
+
+                // Fails if not a PTZ
+                OnvifPtzServiceReference.PTZNode node = ptzService.GetNode("1"); // nodes[0].token);
+
+                PTZConfiguration[] ptzConfigs = ptzService.GetConfigurations();
+                File.AppendAllText("ptz.txt", string.Format("\nPTZ configs found: {0}", ptzConfigs.Length));
+                File.AppendAllText("ptz.txt", string.Format("\nPTZ config - Name: {0}", ptzConfigs[0].Name));
+                File.AppendAllText("ptz.txt", string.Format("\nPTZ config - Token: {0}", ptzConfigs[0].token));
+
+                try
+                {
+                    //PTZConfigurationOptions ptzOptions = ptz.GetConfigurationOptions(ptzConfigs[0].token);
+                    //File.AppendAllText("ptz.txt", string.Format("\nPTZ options - Direction: {0}, ContinuousPanTiltVelocitySpace: {1}", ptzOptions?.PTControlDirection?.Reverse, ptzOptions?.Spaces?.ContinuousPanTiltVelocitySpace[0].XRange.Max));
+                    //File.AppendAllText("ptz.txt", string.Format("\nPTZ options - ContinuousZoomVelocitySpace.URI: {0}", ptzOptions.Spaces.ContinuousZoomVelocitySpace[0].URI));
+
+                    // Add PTZ config to Media profile
+                    //mediaClient.AddPTZConfiguration("Profile1", ptzConfigs[0].token);
+
+                    try
+                    {
+                        PTZSpeed velocity = new PTZSpeed();
+                        File.AppendAllText("ptz.txt", string.Format("\nSetting velocity"));
+
+                        //velocity.Zoom = new Vector1D() { x = ptzOptions.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Max, space = ptzOptions.Spaces.ContinuousZoomVelocitySpace[0].URI }; ;
+                        velocity.PanTilt = new Vector2D() { x = (float)-0.5, y = 0 }; ;
+                        //File.AppendAllText("ptz.txt", string.Format("\nXRange.Max = {0}", velocity.Zoom.x));
+                        //File.AppendAllText("ptz.txt", string.Format("\nContinuousZoomVelocitySpace[0].URI = {0}", velocity.Zoom.space));
+
+                        ptzService.GotoPreset(profileToken, presets[presets.Length - 1].token, velocity);
+
+                        //ptz.ContinuousMove(profileToken, velocity, "");
+                        //ptz.GotoHomePosition(profileToken, velocity);
+
+                        ptzService.Stop(profileToken, true, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        File.AppendAllText("ptz.txt", string.Format("\nException trying to PTZ:\n\t{0}\n\tStack Trace: {1}", ex.Message, ex.StackTrace));
+                        if (ex.InnerException != null)
+                            File.AppendAllText("ptz.txt", string.Format("\n\tInner Exception: {0}", ex.InnerException));
+                        throw;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+                    throw;
+                }
+            }
+        }
+
+        private OnvifMediaServiceReference.MediaClient GetOnvifMediaClient(string ip, int port)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/media_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            OnvifMediaServiceReference.MediaClient mediaClient = new OnvifMediaServiceReference.MediaClient(bind, serviceAddress);
+
+            return mediaClient;
+        }
+
+        private PTZClient GetOnvifPTZClient(string ip, int port)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/ptz_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            PTZClient ptzClient = new PTZClient(bind, serviceAddress);
+
+            return ptzClient;
+        }
+    }
+}
