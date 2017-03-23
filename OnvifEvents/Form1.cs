698917903c0dea3fb921fe051a6df6cd7c284baf
thephez <thephez@users.noreply.github.com>
2017-03-23 08:06:13 -0400

    Added separate project to work on Onvif events  - Basic pull messages (no auth) working on Bosch

---
+++ OnvifEvents/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Net;
+using OnvifEvents.OnvifEventServiceReference;
+using System.Xml;
+
+
+namespace OnvifEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Net.ServicePointManager.Expect100Continue = false;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string ip = "172.16.5.12";
+            int port = 80;
+            //SubscriptionManagerClient smc = new SubscriptionManagerClient(bind, serviceAddress);
+
+            EventPortTypeClient eptc = OnvifServices.GetEventClient(ip, port);
+            
+            // 1. Get Event Properties
+            bool fixedTopicSet;
+            TopicSetType tst = new TopicSetType();
+            string[] topicExpDialect;
+            string[] msgContentFilter;
+            string[] producerProperties;
+            string[] msgContentSchema;
+            XmlElement[] any;
+
+            string[] result = eptc.GetEventProperties(out fixedTopicSet, out tst, out topicExpDialect, out msgContentFilter, out producerProperties, out msgContentSchema, out any);
+
+            listBox1.Items.Add(string.Format("Result: {0}", result));
+            listBox1.Items.Add(string.Format("Message Content Filter Dialect(s)"));
+            foreach (string msg in msgContentFilter)
+                listBox1.Items.Add(string.Format("  {0}", msg));
+
+            listBox1.Items.Add("");
+            listBox1.Items.Add(string.Format("Message Content Schema Location(s)"));
+            foreach (string msg in msgContentSchema)
+                listBox1.Items.Add(string.Format("  {0}", msg));
+
+            listBox1.Items.Add("");
+            listBox1.Items.Add(string.Format("Producer Properties Filter Dialect(s)"));
+            foreach (string msg in producerProperties)
+                listBox1.Items.Add(string.Format("  {0}", msg));
+
+            listBox1.Items.Add("");
+            listBox1.Items.Add(string.Format("Topic Expression Dialect(s)"));
+            foreach (string msg in topicExpDialect)
+                listBox1.Items.Add(string.Format("  {0}", msg));
+
+            listBox1.Items.Add("");
+            listBox1.Items.Add(string.Format("Topic Set item(s)"));
+            foreach (XmlElement x in tst.Any)
+                listBox1.Items.Add("  " + x.Name);
+
+            Capabilities c = eptc.GetServiceCapabilities();
+
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Capabilites");
+            listBox1.Items.Add(string.Format("  MaxNotificationProducers: {0}", c.MaxNotificationProducers));
+            listBox1.Items.Add(string.Format("  MaxNotificationProducersSpecified: {0}", c.MaxNotificationProducersSpecified));
+            listBox1.Items.Add(string.Format("  MaxPullPoints: {0}", c.MaxPullPoints));
+            listBox1.Items.Add(string.Format("  MaxPullPointsSpecified: {0}", c.MaxPullPointsSpecified));
+            listBox1.Items.Add(string.Format("  PersistentNotificationStorage: {0}", c.PersistentNotificationStorage));
+            listBox1.Items.Add(string.Format("  PersistentNotificationStorageSpecified: {0}", c.PersistentNotificationStorageSpecified));
+            listBox1.Items.Add(string.Format("  WSPausableSubscriptionManagerInterfaceSupport: {0}", c.WSPausableSubscriptionManagerInterfaceSupport));
+            listBox1.Items.Add(string.Format("  WSPausableSubscriptionManagerInterfaceSupportSpecified: {0}", c.WSPausableSubscriptionManagerInterfaceSupportSpecified));
+            listBox1.Items.Add(string.Format("  WSPullPointSupport: {0}", c.WSPullPointSupport));
+            listBox1.Items.Add(string.Format("  WSPullPointSupportSpecified: {0}", c.WSPullPointSupportSpecified));
+            listBox1.Items.Add(string.Format("  WSSubscriptionPolicySupport: {0}", c.WSSubscriptionPolicySupport));
+            listBox1.Items.Add(string.Format("  WSSubscriptionPolicySupportSpecified: {0}", c.WSSubscriptionPolicySupportSpecified));
+
+            //pull(ip, port);
+            //subscribe(ip, port);
+            test(ip, port);
+
+            //PullPointClient ppc = OnvifServices.GetPullPointClient(ip, port);
+                        
+
+            //CreatePullPointSubscriptionRequest ppsr = new CreatePullPointSubscriptionRequest();
+            //ppsr.InitialTerminationTime = "PT60S";
+
+            //CreatePullPointClient ppc = new CreatePullPointClient(bind, serviceAddress);
+            //CreatePullPoint pp = new CreatePullPoint();
+            //ppc.CreatePullPoint(pp);
+        }
+
+        private void N_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Console.WriteLine("Notify property changed");
+            throw new NotImplementedException();
+        }
+
+        private void Ppsp_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            listBox1.Items.Add(string.Format("--Property changed: {0}", e.PropertyName));
+            
+        }
+
+        public void subscribe(string ip, int port)
+        {
+            EventPortTypeClient eptc = OnvifServices.GetEventClient(ip, port);
+
+            string localIP = GetLocalIp(); // "172.16.5.111";
+
+            // Consumer client (for receiving notifications?) - not working
+            NotificationConsumerClient ncc = OnvifServices.GetNotificationConsumerClient(localIP, 8085, "subscription-1");
+            Console.WriteLine(string.Format("Consumer Client state: {0}", ncc.State));
+            ncc.Open();
+            Console.WriteLine(string.Format("Consumer Client state: {0}", ncc.State));
+            Notify n = new Notify();
+            n.PropertyChanged += N_PropertyChanged;
+            ncc.Notify(n);
+
+
+            // Producer client
+            NotificationProducerClient npc = OnvifServices.GetNotificationProducerClient(ip, port);
+            npc.Endpoint.Address = eptc.Endpoint.Address;
+
+            Subscribe s = new Subscribe();
+            // Consumer reference tells the device where to Post messages back to (the client)
+            EndpointReferenceType clientEndpoint = new EndpointReferenceType() { Address = new AttributedURIType() { Value = string.Format("http://{0}:8085/subscription-1", localIP) } };
+            s.ConsumerReference = clientEndpoint;// ert;
+            s.InitialTerminationTime = "PT60S";
+
+            SubscribeResponse sr = npc.Subscribe(s);
+
+        }
+
+        public void pull(string ip, int port)
+        {
+            EventPortTypeClient eptc = OnvifServices.GetEventClient(ip, port);
+
+            // 2. CreatePullPointSubscription
+            FilterType filter = new FilterType();
+            DateTime currentTime;
+            XmlElement[] xml = null;
+            DateTime? termTime;
+            List<MessageHeader> lstHeaders = new List<MessageHeader>() { };
+
+            EndpointReferenceType ert = eptc.CreatePullPointSubscription(
+                filter, 
+                "PT15S", 
+                new CreatePullPointSubscriptionSubscriptionPolicy(), 
+                ref xml, 
+                out currentTime, 
+                out termTime
+                );
+            listBox1.Items.Add(ert.Address.Value);
+
+            if ((ert.ReferenceParameters != null) && (ert.ReferenceParameters.Any != null))
+            {
+                foreach (System.Xml.XmlElement oXml in ert.ReferenceParameters.Any)
+                {
+                    string strName = oXml.LocalName;
+                    string strNS = oXml.NamespaceURI;
+                    string strValue = oXml.InnerXml;
+
+                    lstHeaders.Add(MessageHeader.CreateHeader(strName, strNS, strValue, true));
+                }
+            }
+
+            //PullPointSubscriptionClient ppsc = OnvifServices.GetPullPointSubClient(ip, port);
+            //NotificationMessageHolderType[] nmht;
+            //DateTime terminationTime;
+            //XmlDocument doc = new XmlDocument();
+            //doc.CreateElement("test");
+            //XmlElement[] pullMsgXml = { doc.CreateElement("test") };
+
+            //ppsc.PullMessages("PT5S", 5, any, out terminationTime, out nmht);
+
+        }
+
+        public void test(string ip, int port)
+        {
+            XmlElement[] Any = new XmlElement[] { };
+            DateTime CurrentTime;
+            DateTime? TerminationTime;
+            List<MessageHeader> lstHeaders = new List<MessageHeader>() { };
+            var oEventClient = OnvifServices.GetEventClient(ip, port);
+
+            var oAux1 = oEventClient.CreatePullPointSubscription(
+                new FilterType(),
+                "PT600S",
+                new CreatePullPointSubscriptionSubscriptionPolicy(),
+                ref Any,
+                out CurrentTime,
+                out TerminationTime);
+
+            if ((oAux1.ReferenceParameters != null) && (oAux1.ReferenceParameters.Any != null))
+            {
+                foreach (XmlElement oXml in oAux1.ReferenceParameters.Any)
+                {
+                    string strName = oXml.LocalName;
+                    string strNS = oXml.NamespaceURI;
+                    string strValue = oXml.InnerXml;
+
+                    lstHeaders.Add(MessageHeader.CreateHeader(strName, strNS, strValue, true));
+                }
+            }
+            
+            //HttpRequestMessageProperty requestMessage = new HttpRequestMessageProperty();
+            //requestMessage.Headers.Add("test", "value");
+            lstHeaders.Add(MessageHeader.CreateHeader("test", "something", "value"));
+
+            // oAux1.Address.Value -> the proxy endpoint address
+            // lstHeaders -> headers to add to the SOAP message of the proxy request
+            //var oPullPointSubscriptionClient = OnvifServices.GetPullPointSubClient(oAux1.Address.Value, 80, lstHeaders);
+            var oPullPointSubscriptionClient = OnvifServices.GetPullPointSubClient(ip, 80, oAux1.Address.Value, lstHeaders);
+            var oSubscriptionManagerClient = OnvifServices.GetSubscriptionManagerClient(oAux1.Address.Value); //, 80, lstHeaders);
+
+            //do
+            //{
+                NotificationMessageHolderType[] NotificationMessages;
+            try
+            {
+                oPullPointSubscriptionClient.PullMessages("PT60S", 1024, Any, out CurrentTime, out NotificationMessages);
+
+                listBox1.Items.Add("");
+                listBox1.Items.Add("Events");
+                foreach (NotificationMessageHolderType message in NotificationMessages)
+                {
+                    foreach (XmlAttribute x in message.Message.Attributes)
+                    {
+                        Console.WriteLine(string.Format("{0} = {1}", x.Name, x.Value));
+                    }
+                    //listBox1.Items.Add(message.Message.OuterXml.ToString());
+                    string time = message.Message.Attributes[0].Value;
+                    string topic = message.Topic.Any[0].Value;
+                    string operation = message.Message.Attributes[1].Value;
+                    string source = message.Message.ChildNodes[0].Value;
+                    string key;
+                    string data = message.Message.ChildNodes[1].Value;
+                    listBox1.Items.Add(string.Format("  {0}\t{1}\t{2}\t{3}\t{4}", time, topic, operation, source, data));
+
+                }
+
+                //var oRenewResult = oSubscriptionManagerClient.Renew(new LIB.Cameras.ONVIF_WS.onvif10_events.Renew() { TerminationTime = "PT600S" });
+                var oRenewResult = oSubscriptionManagerClient.Renew(new Renew() { TerminationTime = "PT60S" });
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("------");
+                listBox1.Items.Add(string.Format("Exception: {0}", ex.Message));
+            }
+
+            //} while (_session != null);
+
+            //oSubscriptionManagerClient.Unsubscribe(new Unsubscribe());
+        }
+
+
+        public string GetLocalIp()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    return localIP;
+                }
+            }
+
+            throw new Exception("Local IP address not found");
+        }
+    }
+}
---
+++ OnvifEvents/OnvifServices.cs
@@ -0,0 +1,137 @@
+﻿using OnvifEvents.OnvifEventServiceReference;
+using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace OnvifEvents
+{
+    public static class OnvifServices
+    {
+
+        public static EventPortTypeClient GetEventClient(string ip, int port)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/event_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            EventPortTypeClient eptc = new EventPortTypeClient(bind, serviceAddress);
+            return eptc;
+        }
+
+
+        public static PullPointSubscriptionClient GetPullPointSubClient(string ip, int port)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/event_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            PullPointSubscriptionClient ppsc = new PullPointSubscriptionClient(bind, serviceAddress);
+            return ppsc;
+        }
+
+
+        public static PullPointSubscriptionClient GetPullPointSubClient(string ip, int port, string toHeaderAddr,  List<MessageHeader> headers)
+        {
+            //EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/event_service", ip, port));
+            AddressHeader[] addrHeader; // = AddressHeader.CreateAddressHeader("Action", "http://www.w3.org/2005/08/addressing", "http://docs.oasis-open.org/wsn/bw-2/NotificationProducer/SubscribeRequest");
+            addrHeader = new AddressHeader[2];
+            addrHeader[0] = AddressHeader.CreateAddressHeader("Action", "http://www.w3.org/2005/08/addressing", "http://www.onvif.org/ver10/events/wsdl/PullPointSubscription/PullMessagesRequest");
+            addrHeader[1] = AddressHeader.CreateAddressHeader("To", "http://www.w3.org/2005/08/addressing", toHeaderAddr);
+            //addrHeader[1] = AddressHeader.CreateAddressHeader("MessageID", "http://www.w3.org/2005/08/addressing", "urn:uuid:e47b9746-c5c1-455e-839f-0db5bed7d8c7");
+            //addrHeader[1] = AddressHeader.CreateAddressHeader("ReplyTo", "http://www.w3.org/2005/08/addressing", "http://www.w3.org/2005/08/addressing/anonymous");
+
+
+            //MessageHeader[] msgHeader;
+            //msgHeader = new MessageHeader[1];
+            //msgHeader[0] = MessageHeader.CreateHeader("Action", "http://www.w3.org/2005/08/addressing", "http://docs.oasis-open.org/wsn/bw-2/NotificationProducer/SubscribeRequest", true);
+
+            EndpointAddress serviceAddress = new EndpointAddress(
+                new System.Uri(string.Format("http://{0}:{1}/onvif/event_service", ip, port)),
+                addrHeader);
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            PullPointSubscriptionClient ppsc = new PullPointSubscriptionClient(bind, serviceAddress);
+            return ppsc;
+        }
+
+        public static PullPointClient GetPullPointClient(string ip, int port)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/event_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+            
+            PullPointClient client = new PullPointClient(bind, serviceAddress);
+            return client;
+        }
+
+        public static NotificationProducerClient GetNotificationProducerClient(string ip, int port)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/onvif/event_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            NotificationProducerClient client = new NotificationProducerClient(bind, serviceAddress);
+            return client;
+        }
+
+        public static NotificationConsumerClient GetNotificationConsumerClient(string ip, int port, string subscription)
+        {
+
+            EndpointAddress serviceAddress = new EndpointAddress(string.Format("http://{0}:{1}/{2}", ip, port, subscription));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.WSAddressing10);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            NotificationConsumerClient client = new NotificationConsumerClient(bind, serviceAddress);
+            return client;
+        }
+
+
+        //public static SubscriptionManagerClient GetSubscriptionManagerClient(string ip, int port, List<MessageHeader> headers)
+        public static SubscriptionManagerClient GetSubscriptionManagerClient(string uri) // string ip, int port, List<MessageHeader> headers)
+        {
+            EndpointAddress serviceAddress = new EndpointAddress(uri); // string.Format("http://{0}:{1}/onvif/event_service", ip, port));
+
+            HttpTransportBindingElement httpBinding = new HttpTransportBindingElement();
+            httpBinding.AuthenticationScheme = AuthenticationSchemes.Digest;
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None);
+            CustomBinding bind = new CustomBinding(messageElement, httpBinding);
+
+            SubscriptionManagerClient client = new SubscriptionManagerClient(bind, serviceAddress);
+            return client;
+        }
+    }
+}
